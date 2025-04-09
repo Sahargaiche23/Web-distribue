0272e88fff52a63f68435cf39147fb9d20459f02
Sahargaiche23 <sahargaiche6@gmail.com>
2025-04-09 21:02:48 +0100

    EurkaServeur

---
+++ src/main/java/com/example/eurkaserveur/EurkaServeurApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurkaserveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurkaServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurkaServeurApplication.class, args);
+	}
+
+}
