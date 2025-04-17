7ce656d52df4f93af795228116a4b5b485b779e2
Sahargaiche23 <sahargaiche6@gmail.com>
2025-04-17 19:46:27 +0100

    EurkaServeur

--- src/main/java/com/example/eurkaserveur/EurkaServeurApplication.java
+++ src/main/java/com/example/eurkaserveur/EurkaServeurApplication.java
@@ -2,12 +2,14 @@ package com.example.eurkaserveur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurkaServeurApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurkaServeurApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurkaServeurApplication.class, args);
+    }
 
 }
